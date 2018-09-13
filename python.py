dc810d471fd4fe28b673a66f0cd2e1c5aa57e655
Kuroinu61 <43251580+Kuroinu61@users.noreply.github.com>
2018-09-13 17:25:38 -0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,28 @@
+while True:
+    
+    answer = input("Qual a resposta para a vida, o universo... e tudo mais?")
+    
+    if (answer == "42"):
+        break
+    elif(answer == "666"):
+        print("Opa meu consagra...")
+    else:
+        print("Ohhhhhhhhhhhhhhhhhh...")
+
+for count in range(0,10):
+    print("Contagem Regressiva: " + str(count))
+
+# ESSE É UM COMENTÁRIO
+texto = "Eu sou uma string"
+inteiro = 10
+decimal = 3.14159
+boleanas = True
+
+def soma(x, y):
+    resultado = x + y
+    print("O resultado é igual a: " + str(resultado))
+
+
+
+          
+soma(4,5)
